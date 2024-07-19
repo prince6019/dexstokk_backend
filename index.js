5b43c602db7b693e34fb1fb3f84723aa26b7830b
prince6019 <prince.sharma6019@gmail.com>
2024-07-19 12:54:52 +0530

    web url added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "https://dexstokkyam.vercel.app/"],
+    origin: ["http://localhost:5173", "https://dexstokkyam.vercel.app"],
   })
 );
 
