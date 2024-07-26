37d2fbe65018174dbea61831c81aa708de1b623a
prince6019 <prince.sharma6019@gmail.com>
2024-07-26 16:24:11 +0530

    transaction api added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const cors = require("cors");
@@ -8,7 +7,6 @@ const { ethers } = require("ethers");
 const Wallet = require("./models/user");
 const { auth } = require("./middleware/auth");
 
-const { contractAddresses, contractAbi } = require("./constants/contractIndex");
 const transactionRouter = require("./routes/transaction");
 
 const app = express();
@@ -22,9 +20,6 @@ app.use(
 app.use("/sendTransaction", transactionRouter);
 
 const uri = process.env.MONGODB_URI;
-const ALCHEMY_HOLESKY_RPC_URL = process.env.ALCHEMY_HOLESKY_RPC_URL;
-const ALCHEMY_MAINNET_RPC_URL = process.env.ALCHEMY_MAINNET_RPC_URL;
-const ALCHEMY_GNOSIS_RPC_URL = process.env.ALCHEMY_GNOSIS_RPC_URL;
 
 connect = () => {
   mongoose
@@ -62,7 +57,7 @@ app.post("/loginWallet", async (req, res) => {
     const wallet = new ethers.Wallet(privateKey);
     const _address = wallet.address;
 
-    if (address !== address) {
+    if (address !== _address) {
       return res.status(500).json({
         success: false,
         message: "wallet address or private key wrong , Please Try Again",
@@ -71,6 +66,7 @@ app.post("/loginWallet", async (req, res) => {
     const user = await Wallet.findOne({ address });
     let token;
     if (!user) {
+      console.log("user not found");
       const randomBytes = ethers.utils.randomBytes(16);
       const randomAlphanumericString = bytesToAlphanumeric(randomBytes);
       console.log(`Random Alphanumeric String: ${randomAlphanumericString}`);
@@ -85,6 +81,7 @@ app.post("/loginWallet", async (req, res) => {
         expiresIn: "24h",
       });
     } else {
+      console.log("user found");
       token = jwt.sign({ address: user.address }, process.env.JWT_SECRET, {
         expiresIn: "24h",
       });
@@ -105,64 +102,62 @@ app.post("/loginWallet", async (req, res) => {
   }
 });
 
-app.post("/sendTransaction", auth, async (req, res) => {
-  const { address } = req.user;
-  const { functionName, args, chainId } = req.body;
+// app.post("/sendTransaction", auth, async (req, res) => {
+//   const { address } = req.user;
+//   const { functionName, args, chainId } = req.body;
 
-  console.log("Address of wallet:", address);
+//   console.log("Address of wallet:", address);
 
-  try {
-    const user = await Wallet.findOne({ address });
-    if (!user) {
-      return res.status(404).json({ error: "Wallet not found" });
-    }
+//   try {
+//     const user = await Wallet.findOne({ address });
+//     if (!user) {
+//       return res.status(404).json({ error: "Wallet not found" });
+//     }
 
-    const jsonObj = JSON.parse(user.jsonWallet);
-    console.log("JSON File:", jsonObj);
-    let rpc;
-    if (chainId === 1) {
-      rpc = ALCHEMY_MAINNET_RPC_URL;
-    } else if (chainId === 100) {
-      rpc = ALCHEMY_GNOSIS_RPC_URL;
-    } else if (chainId === 11155111) {
-      rpc = ALCHEMY_HOLESKY_RPC_URL;
-    } else {
-      return res.status(400).json({ error: "chainID not supported" });
-    }
-    const provider = new ethers.providers.JsonRpcProvider(rpc);
+//     const jsonObj = JSON.parse(user.jsonWallet);
+//     console.log("JSON File:", jsonObj);
+//     let rpc;
+//     if (chainId === 1) {
+//       rpc = ALCHEMY_MAINNET_RPC_URL;
+//     } else if (chainId === 100) {
+//       rpc = ALCHEMY_GNOSIS_RPC_URL;
+//     } else if (chainId === 11155111) {
+//       rpc = ALCHEMY_HOLESKY_RPC_URL;
+//     } else {
+//       return res.status(400).json({ error: "chainID not supported" });
+//     }
+//     const provider = new ethers.providers.JsonRpcProvider(rpc);
 
-    const wallet = await ethers.Wallet.fromEncryptedJson(
-      jsonFile,
-      user.hashedPassword
-    );
-    const connectedWallet = wallet.connect(provider);
+//     const wallet = await ethers.Wallet.fromEncryptedJson(
+//       jsonFile,
+//       user.hashedPassword
+//     );
+//     const connectedWallet = wallet.connect(provider);
 
-    const contract = new ethers.Contract(
-      contractAddresses[chainId],
-      contractAbi,
-      connectedWallet
-    );
+//     const contract = new ethers.Contract(
+//       contractAddresses[chainId],
+//       contractAbi,
+//       connectedWallet
+//     );
 
-    // Call the function on the smart contract
-    const tx = await contract[functionName](...args);
+//     // Call the function on the smart contract
+//     const tx = await contract[functionName](...args);
 
-    // Wait for the transaction to be mined
-    const receipt = await tx.wait();
+//     // Wait for the transaction to be mined
+//     const receipt = await tx.wait();
 
-    res.status(200).json({
-      message: "Transaction successful",
-      transactionHash: receipt.transactionHash,
-      receipt: receipt,
-    });
-  } catch (error) {
-    console.error("Error sending transaction:", error);
-    res
-      .status(500)
-      .json({ error: "Transaction failed", details: error.message });
-  }
-});
-
-app.post("/sendTransaction");
+//     res.status(200).json({
+//       message: "Transaction successful",
+//       transactionHash: receipt.transactionHash,
+//       receipt: receipt,
+//     });
+//   } catch (error) {
+//     console.error("Error sending transaction:", error);
+//     res
+//       .status(500)
+//       .json({ error: "Transaction failed", details: error.message });
+//   }
+// });
 
 // listening on port 3000-----------------
 app.listen(PORT, () => {
--- routes/transaction.js
+++ routes/transaction.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 require("dotenv").config();
 const Wallet = require("../models/user");
-const { ethers } = require("ethers");
+const { ethers, BigNumber } = require("ethers");
 const { auth } = require("../middleware/auth");
 const {
   contractAddresses,
@@ -9,7 +9,7 @@ const {
 } = require("../constants/contractIndex");
 
 const transactionRouter = Router();
-transactionRouter.use(auth);
+// transactionRouter.use(auth);
 
 const ALCHEMY_HOLESKY_RPC_URL = process.env.ALCHEMY_HOLESKY_RPC_URL;
 const ALCHEMY_MAINNET_RPC_URL = process.env.ALCHEMY_MAINNET_RPC_URL;
@@ -21,7 +21,7 @@ const tokenAbi = [
   "function decimals() external view returns (uint8)",
   "function approve(address spender, uint256 value) external returns (bool)",
   "function balanceOf(address account) external view returns (uint256)",
-  "function allowance(address account)", //todo
+  "function allowance(address owner, address spender) external view returns (uint256)",
 ];
 
 const getContract = async (address, chainId) => {
@@ -31,7 +31,6 @@ const getContract = async (address, chainId) => {
       return res.status(404).json({ error: "Wallet not found" });
     }
     const jsonObj = JSON.parse(user.jsonWallet);
-    console.log("JSON File:", jsonObj);
     let rpc;
     switch (chainId) {
       case 1:
@@ -49,8 +48,8 @@ const getContract = async (address, chainId) => {
 
     const provider = new ethers.providers.JsonRpcProvider(rpc);
     const wallet = await ethers.Wallet.fromEncryptedJson(
-      jsonFile,
-      user.hashedPassword
+      jsonObj,
+      user.password
     );
     const signer = wallet.connect(provider);
     const estokkContract = new ethers.Contract(
@@ -75,10 +74,21 @@ const createOffer = async (req, res) => {
     address,
     chainId,
   } = req.body;
+  console.log(
+    "in creating offer : ",
+    offerType,
+    offerToken,
+    buyerToken,
+    buyerAddress,
+    price,
+    amount,
+    address,
+    chainId
+  );
   try {
-    const { estokkContract, signer } = getContract(address, chainId);
+    const { estokkContract, signer } = await getContract(address, chainId);
     const tokenContract = new ethers.Contract(offerToken, tokenAbi, signer);
-    const allowance = await tokenContract.allowance(
+    let allowance = await tokenContract.allowance(
       address,
       contractAddresses[chainId]
     );
@@ -91,15 +101,25 @@ const createOffer = async (req, res) => {
     const _amountInInt = ethers.utils.formatUnits(amount, buyerTokenDetails); //buyertokendecimals
     const _amountToApproveInInt = _priceInInt * _amountInInt;
     const _amountToApproveInEthers = ethers.utils.parseUnits(
-      _amountToApproveInInt,
+      _amountToApproveInInt.toString(),
       offerTokenDecimals
     );
     const amountToApprove =
       offerType === "buy" ? _amountToApproveInEthers : amount;
-
+    console.log("amount to approve : ", amountToApprove);
+    const newAllowance = allowance.add(amountToApprove);
+    console.log("new allowance : ", parseInt(newAllowance._hex, 16));
+    const balance = await tokenContract.balanceOf(address);
+    console.log("balance of offer token L ", parseInt(balance._hex, 16));
+    if (balance.lt(newAllowance)) {
+      console.log("there is error");
+      return res
+        .status(404)
+        .json({ error: "balance is too low than total amount to approve" });
+    }
     const approveTx = await tokenContract.approve(
       contractAddresses[chainId],
-      allowance + amountToApprove
+      newAllowance
     );
     await approveTx.wait(1);
 
@@ -113,15 +133,34 @@ const createOffer = async (req, res) => {
     await tx.wait(1);
     return res.status(200).json({ message: "success", txHash: tx });
   } catch (e) {
-    return res.status(404).json({ error: "Wallet not found" });
+    console.log(e);
+    return res.status(404).json({ e: e, error: "in  creating offer" });
   }
 };
 
 const buyOffer = async (req, res) => {
-  const { offerType, offerToken, buyerToken, price, amount, address, chainId } =
-    req.body;
+  const {
+    offerType,
+    offerId,
+    offerToken,
+    buyerToken,
+    price,
+    amount,
+    address,
+    chainId,
+  } = req.body;
+  console.log(
+    "in buyOffers : ",
+    offerType,
+    offerToken,
+    buyerToken,
+    price,
+    amount,
+    address,
+    chainId
+  );
   try {
-    const { estokkContract, signer } = getContract(address, chainId);
+    const { estokkContract, signer } = await getContract(address, chainId);
 
     const tokenContract = new ethers.Contract(buyerToken, tokenAbi, signer);
     const allowance = await tokenContract.allowance(
@@ -138,16 +177,24 @@ const buyOffer = async (req, res) => {
     const _amountInInt = ethers.utils.formatUnits(amount, buyerTokenDetails); //buyertokendecimals
     const _amountToApproveInInt = _priceInInt * _amountInInt;
     const _amountToApproveInEthers = ethers.utils.parseUnits(
-      _amountToApproveInInt,
+      _amountToApproveInInt.toString(),
       offerTokenDecimals
     );
 
     const amountToApprove =
       offerType === "buy" ? amount : _amountToApproveInEthers;
+    const newAllowance = allowance.add(amountToApprove);
+    console.log("new allowance : ", parseInt(newAllowance._hex, 16));
 
+    const balance = await tokenContract.balanceOf(address);
+    if (balance.lt(newAllowance)) {
+      return res
+        .status(404)
+        .json({ error: "balance is too low than total amount to approve" });
+    }
     const approveTx = await tokenContract.approve(
       contractAddresses[chainId],
-      allowance + amountToApprove
+      newAllowance
     );
     await approveTx.wait(1);
 
@@ -155,15 +202,34 @@ const buyOffer = async (req, res) => {
     await tx.wait(1);
     return res.status(200).json({ message: "success", txHash: tx });
   } catch (e) {
-    return res.status(404).json({ error: "Wallet not found" });
+    console.log(e);
+    return res.status(404).json({ e: e, error: "Wallet not found" });
   }
 };
 
 const deleteOffer = async (req, res) => {
-  const { offerType, offerToken, buyerToken, price, amount, address, chainId } =
-    req.body;
+  const {
+    offerType,
+    offerId,
+    offerToken,
+    buyerToken,
+    price,
+    amount,
+    address,
+    chainId,
+  } = req.body;
+  console.log(
+    "in delete offers: ",
+    offerType,
+    offerToken,
+    buyerToken,
+    price,
+    amount,
+    address,
+    chainId
+  );
   try {
-    const { estokkContract, signer } = getContract(address, chainId);
+    const { estokkContract, signer } = await getContract(address, chainId);
 
     const tokenContract = new ethers.Contract(offerToken, tokenAbi, signer);
     const allowance = await tokenContract.allowance(
@@ -180,16 +246,23 @@ const deleteOffer = async (req, res) => {
     const _amountInInt = ethers.utils.formatUnits(amount, buyerTokenDetails); //buyertokendecimals
     const _amountToApproveInInt = _priceInInt * _amountInInt;
     const _amountToApproveInEthers = ethers.utils.parseUnits(
-      _amountToApproveInInt,
+      _amountToApproveInInt.toString(),
       offerTokenDecimals
     );
 
     const amountToApprove =
       offerType === "buy" ? _amountToApproveInEthers : amount;
+    const newAllowance = allowance.sub(amountToApprove);
+    const balance = await tokenContract.balanceOf(address);
+    if (balance.lt(newAllowance)) {
+      return res
+        .status(404)
+        .json({ error: "balance is too low than total amount to approve" });
+    }
 
     const approveTx = await tokenContract.approve(
       contractAddresses[chainId],
-      allowance - amountToApprove
+      newAllowance
     );
     await approveTx.wait(1);
 
@@ -197,21 +270,95 @@ const deleteOffer = async (req, res) => {
     await tx.wait(1);
     return res.status(200).json({ message: "success", txHash: tx });
   } catch (e) {
-    return res.status(404).json({ error: "Wallet not found" });
+    console.log(e);
+    return res.status(404).json({ e: e, error: "Wallet not found" });
   }
 };
 
 const updateOffer = async (req, res) => {
-  const { offerType, offerToken, buyerToken, price, amount, address, chainId } =
-    req.body;
+  const {
+    offerType,
+    offerId,
+    offerToken,
+    buyerToken,
+    price,
+    amount,
+    oldPrice,
+    oldAmount,
+    address,
+    chainId,
+  } = req.body;
   try {
-    const { estokkContract, signer } = getContract(address, chainId);
+    console.log(
+      "in updateOffer : ",
+      offerType,
+      offerToken,
+      buyerToken,
+      price,
+      amount,
+      oldPrice,
+      oldAmount,
+      address,
+      chainId
+    );
+    const { estokkContract, signer } = await getContract(address, chainId);
+
+    const tokenContract = new ethers.Contract(offerToken, tokenAbi, signer);
+    const allowance = await tokenContract.allowance(
+      address,
+      contractAddresses[chainId]
+    );
+    console.log("allowance :", parseInt(allowance._hex, 16));
+
+    const offerTokenInfo = await estokkContract.tokenInfo(offerToken);
+    const buyerTokenInfo = await estokkContract.tokenInfo(buyerToken);
+    const offerTokenDecimals = parseInt(offerTokenInfo[0]._hex, 16);
+    const buyerTokenDetails = parseInt(buyerTokenInfo[0]._hex, 16);
+    const _priceInInt = ethers.utils.formatUnits(price, offerTokenDecimals); //offertokendecimals
+    const _amountInInt = ethers.utils.formatUnits(amount, buyerTokenDetails); //buyertokendecimals
+    const _amountToApproveInInt = _priceInInt * _amountInInt;
+    const _amountToApproveInEthers = ethers.utils.parseUnits(
+      _amountToApproveInInt.toString(),
+      offerTokenDecimals
+    );
+    const _amountToSubtractInInt =
+      offerType === "buy" ? oldPrice * oldAmount : oldAmount;
+    const _amountToSubtractInEthers = ethers.utils.parseUnits(
+      _amountToSubtractInInt.toString(),
+      offerTokenDecimals
+    );
+    console.log(
+      "amount to subtract: ",
+      parseInt(_amountToSubtractInEthers._hex, 16)
+    );
+
+    const amountToApprove =
+      offerType === "buy" ? _amountToApproveInEthers : amount;
+
+    console.log("amount to approve : ", amountToApprove);
+    const newAllowance = allowance
+      .add(amountToApprove)
+      .sub(_amountToSubtractInEthers);
+
+    const balance = await tokenContract.balanceOf(address);
+    if (balance.lt(newAllowance)) {
+      return res
+        .status(404)
+        .json({ error: "balance is too low than total amount to approve" });
+    }
+    console.log("new allowance : ", parseInt(newAllowance._hex, 16));
+    const approveTx = await tokenContract.approve(
+      contractAddresses[chainId],
+      newAllowance
+    );
+    await approveTx.wait(1);
 
     const tx = await estokkContract.updateOffer(offerId, price, amount);
     await tx.wait(1);
     return res.status(200).json({ message: "success", txHash: tx });
   } catch (e) {
-    return res.status(404).json({ error: "Wallet not found" });
+    console.log(e);
+    return res.status(404).json({ e: e, error: "Wallet not found" });
   }
 };
 
